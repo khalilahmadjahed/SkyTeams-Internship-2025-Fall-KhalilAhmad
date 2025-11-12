d5b08c781fe57cd54eb4dc0405e5ca8da5cb853f
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-12 12:05:30 +0430

    question_1_test successfully runed

---
+++ week_2/question_1_test.java
@@ -0,0 +1,11 @@
+package week_2;
+
+public class question_1_test {
+    public static void main(String[] args) {
+        question_1.swapVariables(5, 3);
+        question_1.swapVariables(-1, -3);
+        question_1.swapVariables(8, -4);
+        question_1.swapVariables(0, 3);
+        question_1.swapVariables(-6, 0);
+    }
+}

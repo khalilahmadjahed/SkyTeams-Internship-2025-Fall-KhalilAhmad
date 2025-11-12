e59d0f386242176230d3184d2cf6af46e154a13c
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-12 12:21:36 +0430

    question_3_test successfully runed

---
+++ week_2/question_3_test.java
@@ -0,0 +1,8 @@
+package week_2;
+
+public class question_3_test {
+    public static void main(String[] args) {
+        double[] areaPerimeter = question_3.areaPerimeter(4, 6);
+        System.out.println("Area = " + areaPerimeter[0] +" / Perimeter = " + areaPerimeter[1]);
+    }
+}

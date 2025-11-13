11254d0accd42e8a1774d582b6604e785f07b94d
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-13 10:55:47 +0430

    question_7_test runed successfully

---
+++ week_2/question_7_test.java
@@ -0,0 +1,10 @@
+package week_2;
+
+public class question_7_test {
+    public static void main(String[] args) {
+        System.out.println(question_7.checkEvenOdd(4));
+        System.out.println(question_7.checkEvenOdd(5));
+        System.out.println(question_7.checkEvenOdd(-4));
+    
+    }
+}

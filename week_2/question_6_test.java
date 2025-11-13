6d4f2268f0a8a5e10d51e5684a117ef51b492b90
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-13 09:34:49 +0430

    question_6_test runed successfully

--- week_2/question_6_test.java
+++ week_2/question_6_test.java
@@ -2,7 +2,19 @@ package week_2;
 
 public class question_6_test {
     public static void main(String[] args) {
-        String[] convertTimeSections = question_6.convertHrMinSec(3665);
-        System.out.println(convertTimeSections[0] +  convertTimeSections[1] + convertTimeSections[2]);
+        
+        double[] convertTimeSections = question_6.convertHrMinSec(3665);
+        if (convertTimeSections[0] == -1) {
+            System.out.println("Enter valid seconds!");
+        }else{
+            System.out.println("hr = " + convertTimeSections[0] +  " / min = " + convertTimeSections[1] + " / sec " + convertTimeSections[2]);
+        }
+
+        convertTimeSections = question_6.convertHrMinSec(-3665);
+        if (convertTimeSections[0] == -1) {
+            System.out.println("Enter valid seconds!");
+        }else{
+            System.out.println("hr = " + convertTimeSections[0] +  " / min = " + convertTimeSections[1] + " / sec " + convertTimeSections[2]);
+        }
     }
 }

fa179decf329ce1b3bdb162c53ee02d9bc04ff89
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-13 11:17:31 +0430

    queastion_9_test is runed successfully

---
+++ week_2/question_9_test.java
@@ -0,0 +1,10 @@
+package week_2;
+
+public class question_9_test {
+    public static void main(String[] args) {
+        System.out.println(question_9.calcGrade(100));
+        System.out.println(question_9.calcGrade(55));
+        System.out.println(question_9.calcGrade(-85));
+        System.out.println(question_9.calcGrade(0));
+    }
+}

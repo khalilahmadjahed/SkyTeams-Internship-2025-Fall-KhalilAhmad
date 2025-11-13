17f9a39c88e17634a2b5b0ceeb0cf28b79512a22
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-13 11:04:41 +0430

    question_8_test runed successfully

---
+++ week_2/question_8_test.java
@@ -0,0 +1,9 @@
+package week_2;
+
+public class question_8_test {
+    public static void main(String[] args) {
+        System.out.println(question_8.checkPosNegZero(5));
+        System.out.println(question_8.checkPosNegZero(-8));
+        System.out.println(question_8.checkPosNegZero(0));
+    }
+}

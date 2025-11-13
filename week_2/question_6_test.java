f9fa96d27e666acd2ad3e0e279c63192a75ae8d8
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-13 09:21:04 +0430

    question_6_test runed successfully

---
+++ week_2/question_6_test.java
@@ -0,0 +1,8 @@
+package week_2;
+
+public class question_6_test {
+    public static void main(String[] args) {
+        String[] convertTimeSections = question_6.convertHrMinSec(3665);
+        System.out.println(convertTimeSections[0] +  convertTimeSections[1] + convertTimeSections[2]);
+    }
+}

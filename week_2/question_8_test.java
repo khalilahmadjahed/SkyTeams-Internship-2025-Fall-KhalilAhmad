0bd2c729368763a3d17e3b36f45b643f8bccc946
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-15 17:03:36 +0430

    delete week_2 from main

--- week_2/question_8_test.java
+++
@@ -1,9 +0,0 @@
-package week_2;
-
-public class question_8_test {
-    public static void main(String[] args) {
-        System.out.println(question_8.checkPosNegZero(5));
-        System.out.println(question_8.checkPosNegZero(-8));
-        System.out.println(question_8.checkPosNegZero(0));
-    }
-}

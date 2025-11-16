0d63b392b224436b88f40e70c46ade6fc65ed59c
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-16 14:09:23 +0430

    question_14_test ruend successfully

---
+++ week_2/question_14_test.java
@@ -0,0 +1,23 @@
+package week_2;
+
+public class question_14_test {
+    public static void main(String[] args) {
+        String result = question_14.checkChar("1");
+        System.out.println(result);
+
+        result = question_14.checkChar("c");
+        System.out.println(result);
+
+        result = question_14.checkChar("/");
+        System.out.println(result);
+
+        result = question_14.checkChar("a");
+        System.out.println(result);
+
+        result = question_14.checkChar("t");
+        System.out.println(result);
+
+        result = question_14.checkChar("-");
+        System.out.println(result);
+    }
+}

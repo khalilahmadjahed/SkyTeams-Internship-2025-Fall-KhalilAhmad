e4e2a6ffb646382be76c966157ffa486b4308375
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-17 14:52:15 +0430

    question_23_test runed successfully

---
+++ week_2/question_23_test.java
@@ -0,0 +1,11 @@
+package week_2;
+import java.util.List;
+
+public class question_23_test {
+    public static void main(String[] args) {
+        List<Integer> numbers = question_23.findFibonacci(1);
+        for(int i : numbers){
+            System.out.println(i);
+        }
+    }
+}

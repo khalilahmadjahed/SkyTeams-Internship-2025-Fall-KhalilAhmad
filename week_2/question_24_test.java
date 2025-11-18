3c37acf1698b21173c24b9bb78aff53ead11ac0b
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-11-18 12:55:28 +0430

    question_24_test runed successfully

---
+++ week_2/question_24_test.java
@@ -0,0 +1,9 @@
+package week_2;
+
+public class question_24_test {
+    public static void main(String[] args) {
+        System.out.println("GCD is------------------" +question_24.findGCD(12, 18));
+        System.out.println("GCD is------------------" +question_24.findGCD(18, 12));
+        System.out.println("GCD is------------------" +question_24.findGCD(12, 4));
+    }
+}
